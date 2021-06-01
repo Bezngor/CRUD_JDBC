24e6a710708d60b41c22b540f2fca927c3e212c8
Vladimir Beznossov <bezngor@gmail.com>
2021-06-01 15:06:05 +0300

    Fixing unit-tests for

--- src/test/java/com/bezngor/crud_jdbc/controller/SkillControllerTest.java
+++ src/test/java/com/bezngor/crud_jdbc/controller/SkillControllerTest.java
@@ -3,7 +3,6 @@ package com.bezngor.crud_jdbc.controller;
 import com.bezngor.crud_jdbc.model.Skill;
 import com.bezngor.crud_jdbc.repository.JavaIOSkillRepositoryImpl;
 import com.bezngor.crud_jdbc.repository.SkillRepository;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -15,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SkillControllerTest {
     private SkillRepository mockedRepository;
-    private JavaIOSkillRepositoryImpl mockedIOSkill;
+    private SkillController mockedController;
     private Skill skill;
     private String name;
 
@@ -23,8 +22,8 @@ class SkillControllerTest {
 
     @BeforeEach
     void setUp() {
-        mockedRepository = Mockito.mock(SkillRepository.class);
-        mockedIOSkill = Mockito.mock(JavaIOSkillRepositoryImpl.class);
+        mockedRepository = Mockito.mock(JavaIOSkillRepositoryImpl.class);
+        mockedController = Mockito.mock(SkillController.class);
         name = "Java";
         skill = new Skill(name);
     }
@@ -37,30 +36,31 @@ class SkillControllerTest {
     @Test
     void create() {
         Mockito.when(mockedRepository.save(new Skill(name))).thenReturn(skill);
+        assertSame(mockedController.create(name), skill);
     }
 
     @Test
     void update() {
-        Mockito.when(mockedRepository.update(skill)).thenReturn(skill);
+        Mockito.when(mockedRepository.update(new Skill(0, name))).thenReturn(skill);
+        assertSame(mockedController.update(0,name), skill);
     }
 
     @Test
     void getAll() {
         List<Skill> skills = new ArrayList<>();
-        skills.add(skill);
-        Mockito.when(mockedIOSkill.getAll()).thenReturn(skills);
-        assertEquals(mockedRepository.getAll().size(), 1);
+        skills.add(new Skill(name));
+        Mockito.when(mockedRepository.getAll()).thenReturn(skills);
+        assertSame(mockedController.getAll(), skills);
     }
 
     @Test
     void getById() {
-        List<Skill> skills = new ArrayList<>();
-        skills.add(skill);
-        Mockito.when(mockedIOSkill.getById(0)).thenReturn(skill);
-        assertEquals(mockedRepository.getById(0).getName(), "Java");
+        Mockito.when(mockedRepository.getById(0)).thenReturn(skill);
+        assertSame(mockedController.getById(0), skill);
     }
 
     @Test
     void deleteById() {
+
     }
 }
