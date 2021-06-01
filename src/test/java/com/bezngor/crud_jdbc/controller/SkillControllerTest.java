b11b1475dd439aa9d08b63d6fbc417485ea7bb0c
Vladimir Beznossov <bezngor@gmail.com>
2021-06-01 20:11:32 +0300

    Fixing errors of testing SkillController

--- src/test/java/com/bezngor/crud_jdbc/controller/SkillControllerTest.java
+++ src/test/java/com/bezngor/crud_jdbc/controller/SkillControllerTest.java
@@ -24,8 +24,8 @@ class SkillControllerTest {
     void setUp() {
         mockedRepository = Mockito.mock(JavaIOSkillRepositoryImpl.class);
         controller = new SkillController();
-        name = "Java";
-        skill = new Skill(name);
+        name = "C++";
+        skill = new Skill(1, name);
     }
 
     @Test
@@ -35,28 +35,28 @@ class SkillControllerTest {
 
     @Test
     void create() {
-        Mockito.when(mockedRepository.save(new Skill(name))).thenReturn(skill);
+        Mockito.when(mockedRepository.save(skill)).thenReturn(skill);
         assertEquals(controller.create(name).getName(), name);
     }
 
     @Test
     void update() {
-        Mockito.when(mockedRepository.update(new Skill(0, name))).thenReturn(skill);
-        assertSame(controller.update(0,name).getName(), name);
+        Mockito.when(mockedRepository.update(skill)).thenReturn(skill);
+        assertSame(controller.update(1,name).getName(), name);
     }
 
     @Test
     void getAll() {
         List<Skill> skills = new ArrayList<>();
-        skills.add(new Skill(name));
+        skills.add(new Skill(1, name));
         Mockito.when(mockedRepository.getAll()).thenReturn(skills);
-        assertSame(controller.getAll(), skills);
+        assertEquals(controller.getAll(), skills);
     }
 
     @Test
     void getById() {
-        Mockito.when(mockedRepository.getById(0)).thenReturn(skill);
-        assertSame(controller.getById(0), skill);
+        Mockito.when(mockedRepository.getById(1)).thenReturn(skill);
+        assertEquals(controller.getById(1).getName(), skill.getName());
     }
 
     @Test
